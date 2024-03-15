f9d66b1519ec7dc8a8ac0620706b730ef15f8d06
Harry <harsagra3478@gmail.com>
2024-03-16 00:52:02 +0530

    changed alert page

--- src/Alert/Alert.js
+++ src/Alert/Alert.js
@@ -62,29 +62,23 @@ function Alert() {
 
         <div className="alert bg-slate-200 h-screen m-2">
           <div className="upper-section items-center gap-0">
-            <div className="profile">
+            <div className="flex items-center gap-4">
               {/* <h2>Profile of the student</h2> */}
-              <div className="profile-content flex items-center">
-              
-                <img src={alertData.user.imageUrl} alt="Photo" />
-                <div className="mx-3">
-                  <p className="font-semibold text-lg ">{alertData.user.username}</p>
-                  <p className="font-semibold text-lg ">{alertData.user.rollNo}</p>
-                  <p className="font-semibold text-lg ">{alertData.user.email} </p>
-                  <p className="font-semibold text-lg ">{alertData.user.phone}</p>
+              <div className="profile-content flex items-center justify-center">
+                <img src={alertData.user.imageUrl} alt="Photo" className="rounded-lg p-2" style={{width:'40%', height:'37%', borderRadius:'56px'}}/>
+                <div className="mx-5">
+                  <p className="font-semibold text-lg ">Name: {alertData.user.username}</p>
+                  <p className="font-semibold text-lg ">Roll No: {alertData.user.rollNo}</p>
+                  <p className="font-semibold text-lg ">Email: {alertData.user.email} </p>
+                  <p className="font-semibold text-lg ">Phone: {alertData.user.phone}</p>
+                  <p className="font-semibold text-lg  capitalize p-1">Near: {alertData.landmark}</p>
+                <p className="font-semibold text-lg  ">Date: {alertData.createdOn.split("T")[0]}</p>
+                <p className="font-semibold text-lg ">Time: {alertData.createdOn.split("T")[1].split(".")[0]}</p>
                 </div>
               </div>
             </div>
-            <div>
-              {/* <h2>Case Details</h2> */}
-              <div className="case-details">
-                <p></p>
-                <p className="font-semibold text-2xl capitalize p-1">{alertData.landmark}</p>
-                <p className="font-semibold text-lg p-1">{alertData.createdOn.split("T")[0]}</p>
-                <p className="font-semibold text-lg p-1">{alertData.createdOn.split("T")[1].split(".")[0]}</p>
-              </div>
-            </div>
-            {alertData.user.contacts.map((contact) => {
+  
+            {/* {alertData.user.contacts.map((contact) => {
                                 return (
                                     <div className='emergency-contacts flex items-center gap-2'>
                                         <div className='m-2'>
@@ -96,12 +90,29 @@ function Alert() {
                                         </div>
                                     </div>
                                 )
+                            })} */}
+           <div className='student-contacts min-h-full justify-start flex flex-col items-start' style={{minWidth:'24%'}}>
+                            <h1 className='text-2xl text-center font-bold'>Emergency Contacts</h1>
+                            {alertData.user.contacts.map((contact) => {
+                                return (
+                                    <div className='emergency-contacts text-md flex items-center  gap-1 w-full'>
+                                        <div className='m-0.5'>
+                                            <ContactEmergency className='text-3xl' fontSize='large' />
+                                        </div>
+                                        <div className='m-0.5 p-2'>
+                                            <p><PermIdentity />  {contact.contactName}</p>
+                                            <p><Phone />  {contact.contactPhone}</p>
+                                        </div>
+                                    </div>
+                                )
                             })}
-            <div className="timer">
+                        </div>
+
+            <div className="timer p-2">
               <h2 style={{ color: "#FD0606", fontWeight: "bold" }}>
-                <span style={{ fontSize: "100px" }}>{time}</span> seconds
+                <span style={{ fontSize: "70px" }}>{Math.floor(time/60) <10 ? '0' + Math.floor(time/60) : Math.floor(time/60)}:{Math.floor(time%60) <10 ? '0' + Math.floor(time%60) : Math.floor(time%60)}</span>Mins
               </h2>
-              <p>Time since alert taken</p>
+              <p className="font-semibold text-xl">Time since alert taken</p>
             </div>
           </div>
 
